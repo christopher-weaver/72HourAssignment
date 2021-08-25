a647e1fa5359ac86e1aae5cbeee74356dcc6939a
Christopher Weaver <christopher.m.weaver@outlook.com>
2021-08-24 23:34:25 -0400

    Post controller, services, & models - Night 1

--- 72HourAssignment.API/Controllers/PostController.cs
+++ 72HourAssignment.API/Controllers/PostController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using _72HourAssignment.Models.Models;
+using _72HourAssignment.Services.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,21 +10,72 @@ using System.Web.Http;
 
 namespace _72HourAssignment.API.Controllers
 {
+    [Authorize]
     public class PostController : ApiController
     {
         // POST(Create) a Post(required)
+        [HttpPost]
+        public IHttpActionResult Post(PostCreate post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
+            var postService = CreatePostService();
+
+            if (!postService.CreatePost(post))
+            {
+                return InternalServerError();
+            }
+
+            return Ok($"The following post has been created: {post.Title}");
+        }
 
         // GET All Posts(required)
-
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var posts = postService.GetPosts();
+            return Ok(posts);
+        }
 
         // GET Posts By Author Id
+        // + Any Author Id
+        [Route("api/Post/{authorId}")]
+        public IHttpActionResult Get([FromUri]string authorId)
+        {
+            if (!Guid.TryParse(authorId, out Guid authorGuid))
+            {
+                return BadRequest("Incorrect author ID provided.");
+            }
 
+            PostService postService = CreatePostService();
+            var posts = postService.GetPostsByAuthorId(authorGuid);
+            return Ok(posts);
+        }
+        // + Current user
+        [Route("api/Post/GetAllFromCurrentUser")]
+        public IHttpActionResult GetAllFromCurrentUser()
+        {
+            PostService postService = CreatePostService();
+            var posts = postService.GetPostsFromCurrentUser();
+            return Ok(posts);
+        }
 
         // PUT(Update) a Post
 
 
         // DELETE a Post
 
+
+        // Helper Methods
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var postService = new PostService(userId);
+            return postService;
+        }
     }
 }
---
+++ 72HourAssignment.Services/Services/PostService.cs
@@ -0,0 +1,79 @@
+﻿using _72HourAssignment.Data;
+using _72HourAssignment.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _72HourAssignment.Services.Services
+{
+    public class PostService
+    {
+        private readonly Guid _authorId;
+
+        public PostService(Guid authorId)
+        {
+            _authorId = authorId;
+        }
+
+        public bool CreatePost(PostCreate model)
+        {
+            var newNote =
+                new Post()
+                {
+                    AuthorId = _authorId,
+                    Title = model.Title,
+                    Text = model.Text
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Posts.Add(newNote);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<PostDisplay> GetPosts()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Posts
+                               .Select(p => new PostDisplay
+                               {
+                                   Id = p.ID,
+                                   AuthorId = p.AuthorId,
+                                   Title = p.Title,
+                                   Text = p.Text,
+                                   Comments = p.Comments
+                               });
+
+                return query.ToArray();
+            }
+        }
+
+        public IEnumerable<PostDisplay> GetPostsFromCurrentUser()
+        {
+            return GetPostsByAuthorId(_authorId);
+        }
+
+        public IEnumerable<PostDisplay> GetPostsByAuthorId(Guid authorId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Posts
+                               .Where(p => p.AuthorId == authorId)
+                               .Select(p => new PostDisplay
+                                            {
+                                                Id = p.ID,
+                                                AuthorId = p.AuthorId,
+                                                Title = p.Title,
+                                                Text = p.Text,
+                                                Comments = p.Comments
+                                            });
+
+                return query.ToArray();
+            }
+        }
+    }
+}
