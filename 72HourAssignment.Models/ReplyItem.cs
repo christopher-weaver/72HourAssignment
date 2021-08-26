bce73d0fa1a68129a1d36b8b101b5bdb8af90ae6
Shelby Simpson <shelbycam78@gmail.com>
2021-08-26 15:58:41 -0400

    reply model created

---
+++ 72HourAssignment.Models/ReplyItem.cs
@@ -0,0 +1,27 @@
+﻿using _72HourAssignment.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _72HourAssignment.Models
+{
+    public class RelplyCreate
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Comment))]
+        public int CommentId { get; set; }
+        public virtual Comment Comment { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public Guid AuthorId { get; set; }
+    }
+}
